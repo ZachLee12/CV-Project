0a2c9a904038086ddd056719e4005f777c38f832
ZachLee12 <leezhengyang22@gmail.com>
2023-01-29 20:33:35 +0100

    Add Controlled Components in Address Form

--- src/Components/PersonalDetails/Address.jsx
+++ src/Components/PersonalDetails/Address.jsx
@@ -5,17 +5,76 @@ class Address extends React.Component {
         super(props)
 
         this.state = {
-            street: '',
-            zip: '',
-            city: ''
+            street: 'Street',
+            zip: 'Zip',
+            city: 'City',
+            displayStreet: false,
+            displayZip: false,
+            displayCity: false
         }
+    }
+
+    onChangeStreet = (e) => {
+        this.setState({
+            street: e.target.value
+        })
+    }
+
+    onChangeZip = (e) => {
+        this.setState({
+            zip: e.target.value
+        })
+    }
+
+    onChangeCity = (e) => {
+        this.setState({
+            city: e.target.value
+        })
+    }
+
+    onClickCity = (e) => {
+        this.setState({
+            displayCity: !this.state.displayCity
+        })
+    }
+
+    onClickStreet = (e) => {
+        this.setState({
+            displayStreet: !this.state.displayStreet
+        })
+    }
+
+    onClickZip = (e) => {
+        this.setState({
+            displayZip: !this.state.displayZip
+        })
     }
 
     render() {
         return (
             <div>
-                <p>{this.state.street}</p>
-                {this.state.zip} {this.state.city}
+                <form action="" className="address-form">
+                    <label onClick={this.onClickStreet} htmlFor="street-input">{this.state.street === '' ? 'Street' : this.state.street}</label>
+                    <input
+                        className={`street-input ${this.state.displayStreet ? '' : 'hidden'}`}
+                        onChange={this.onChangeStreet}
+                        type="text"
+                        id="street-input" />
+
+                    <label onClick={this.onClickZip} htmlFor="zip-input">{this.state.zip === '' ? 'Zip' : this.state.zip}</label>
+                    <input
+                        className={`zip-input ${this.state.displayZip ? '' : 'hidden'}`}
+                        onChange={this.onChangeZip}
+                        type="text"
+                        id="zip-input" />
+
+                    <label onClick={this.onClickCity} htmlFor="city-input">{this.state.city === '' ? 'City' : this.state.city}</label>
+                    <input
+                        className={`city-input ${this.state.displayCity ? '' : 'hidden'}`}
+                        onChange={this.onChangeCity}
+                        type="text"
+                        id="city-input" />
+                </form>
             </div>
         )
     }
