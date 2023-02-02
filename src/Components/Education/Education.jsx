5e157df087edf4fca81574bc703be5972a9b36c4
ZachLee12 <leezhengyang22@gmail.com>
2023-02-02 17:53:26 +0100

    Add functions to Education component

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -24,6 +24,52 @@ class Education extends React.Component {
         })
     }
 
+    onChangeInstitution = (e) => {
+        this.setState({
+            education: {
+                ...this.state.education,
+                institution: e.target.value,
+            }
+        })
+    }
+
+    onChangeDuration = (e) => {
+        this.setState({
+            education: {
+                ...this.state.education,
+                duration: e.target.value
+            }
+        })
+    }
+
+    onClickSave = (e) => {
+        this.setState({
+            education: {
+                institution: '',
+                duration: '',
+                id: uniqid()
+            },
+            educationList: [...this.state.educationList, this.state.education],
+            displayEducationForm: false,
+        })
+    }
+
+    onClickCancel = (e) => {
+        this.setState({
+            displayEducationForm: !this.state.displayEducationForm
+        })
+    }
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+    }
+
+    onClickDelete = (e) => {
+        this.setState({
+            educationList: this.state.educationList.filter(education => education.id !== e.target.id)
+        })
+    }
+
     render() {
         return (
             <div>
@@ -39,27 +85,33 @@ class Education extends React.Component {
                     </button>
                 </p>
                 <ul className="education-list">
-                    {this.state.educationList.map(skill => {
+                    {this.state.educationList.map(education => {
                         return (
-                            <li>{skill.title}
+                            <li key={education.id}>{education.institution}
+                                {education.duration}
+                                <button onClick={this.onClickDelete} id={education.id}>x</button>
                                 <Description />
                             </li>
                         )
                     })}
                 </ul>
 
-                <form action="" className={`education-form ${this.state.displayEducationForm? '' : 'hidden'}`}>
+                <form onSubmit={this.onSubmitForm} action="" className={`education-form ${this.state.displayEducationForm ? '' : 'hidden'}`}>
                     <label htmlFor="institution-input">Institution</label>
-                    <input id='institution-input' type="text" />
+                    <input
+                        onChange={this.onChangeInstitution}
+                        id='institution-input'
+                        type="text" />
 
                     <label htmlFor="institution-input">Duration</label>
-                    <input id='institution-input' type="text" />
+                    <input
+                        onChange={this.onChangeDuration}
+                        id='institution-input'
+                        type="text" />
 
-                    <button id='save-button' >Save</button>
-                    <button id='cancel-button' >Cancel</button>
+                    <button onClick={this.onClickSave} id='save-button' >Save</button>
+                    <button onClick={this.onClickCancel} id='cancel-button' >Cancel</button>
                 </form>
-
-
             </div>
         )
     }
