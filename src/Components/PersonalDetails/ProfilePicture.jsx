7228f4c1ada1c0960eb621f2204c1e660439e9bb
ZachLee12 <leezhengyang22@gmail.com>
2023-02-08 19:10:18 +0100

    Refactor base64 to private function

--- src/Components/PersonalDetails/ProfilePicture.jsx
+++ src/Components/PersonalDetails/ProfilePicture.jsx
@@ -9,27 +9,33 @@ class ProfilePicture extends React.Component {
         }
     }
 
-    onChangeInput = (e) => {
-        new Promise(resolve => {
+
+    onChangeInput = async (e) => {
+        //await Promise to resolve first,
+        const base64 = await this.#getImageBase64(e.target.files[0])
+        //then only call this.setState()
+        this.setState({
+            imageBase64: base64
+        })
+    }
+
+    #getImageBase64 = async (file) => {
+        return new Promise(resolve => {
             const reader = new FileReader();
-            reader.readAsDataURL(e.target.files[0])
+            reader.readAsDataURL(file)
             reader.onloadend = function () {
                 resolve(reader.result) //base64
             }
-        })
-            .then((base64) => {
-                this.setState({
-                    imageBase64: base64
-                })
-            }).catch(err => console.log('No change in uploaded image, no re-rendering needed'))
+        }).catch(err => console.log('No change in uploaded image, no re-rendering needed'))
     }
 
     render() {
         return (
-            <div>
+            <div id="ProfilePicture">
                 <img className="profile-picture" src={this.state.imageBase64 === '' ? PlaceholderImage : this.state.imageBase64} alt="profile" />
-                <label>Your Profile Picture
+                <label className="upload-input-label-and-wrapper">Upload a profile picture
                     <input
+                        className="file-input"
                         onChange={this.onChangeInput}
                         type="file" id="myFile" name="filename" accept="image/png, image/gif, image/jpeg, image/webp, image/jpg" />
                 </label>
