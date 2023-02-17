7d1a0429a0e65cdc9baa03c0bc1cd5d031e30bca
ZachLee12 <leezhengyang22@gmail.com>
2023-02-17 23:01:48 +0100

    Update PersonalDetails test

--- src/__tests__/PersonalDetails.test.js
+++ src/__tests__/PersonalDetails.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getByTestId, getByText, render, screen } from '@testing-library/react'
+import {render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import '@testing-library/user-event'
 import PersonalDetails from "../Components/PersonalDetails/PersonalDetails";
